3efe1253e1e31f01b45a1368ff084dacc75d5f6a
schne867 <schne867@gmail.com>
2021-07-12 16:40:35 -0400

    Version1
    First Save

---
+++ Assets/Scripts/BattleHex.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleHex : MonoBehaviour
+{
+
+    [SerializeField] int horizonalCoordinator;
+    [SerializeField] int verticalCoordinator;
+
+
+    private void OnJointBreak(float breakForce)
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
